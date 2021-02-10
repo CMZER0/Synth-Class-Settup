1ebf605dd923280d01ad823957e2e9940a6a52f0
Cole Merchant <xcmzer0x@gmail.com>
2021-02-09 19:09:30 -0700

    Background & Image 1

--- OscillatorPanel.java
+++ OscillatorPanel.java
@@ -1,38 +1,51 @@
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.event.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.BasicStroke;
 
-public class OscillatorPanel extends JPanel {
+public class OscillatorPanel extends JPanel implements ActionListener {
     Dimension oscDimension = new Dimension(500, 350);
+    Timer timer;
     Oscillator a = new Carrier();
+    int xPos = 0;
+    int velocity = 5;
     Color c;
 
     OscillatorPanel(Color c) {
-        //
-        ImageIcon icon = new ImageIcon("ZNH.png");
-        //
-        JLabel label = new JLabel();
-        label.setText(a.type);
-        label.setIcon(icon);
         // Init JPanel
-        this.setPreferredSize(oscDimension);
-        this.setBackground(Color.GRAY);
-        // Add Label
-        this.add(label);
         this.c = c;
+        this.setVisible(true);
+        this.setPreferredSize(oscDimension);
+        this.setBackground(c);
+
+        timer = new Timer(25, this);
+        timer.start();
     }
 
+    @Override
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
         g2D.setColor(c);
         g2D.fillRect(0, 0, oscDimension.width, oscDimension.height);
+        g2D.setColor(Color.BLACK);
+        g2D.setStroke(new BasicStroke(5));
+        g2D.drawLine(0, oscDimension.height / 2, xPos, oscDimension.height / 2);
+
         // g2D.drawBytes(a.multiSample(a.samples), 0, (int) ((LFO) a).samplePeriod,
         // (int) oscDimension.getWidth() / 2,
-        // (int) oscDimension.getHeight() / 2);
+        // int) oscDimension.getHeight() / 2);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (xPos >= oscDimension.width)
+            xPos = 0;
+        else
+            xPos += velocity;
+        this.repaint();
     }
 }
--- PluginWindow.java
+++ PluginWindow.java
@@ -7,6 +7,7 @@ public class PluginWindow extends JFrame implements Runnable {
     protected OscillatorPanel oscOne;
     protected OscillatorPanel oscTwo;
     ImageIcon icon;
+    Image backGround;
     JLabel label;
     Dimension windowDimension = new Dimension(1045, 800);
     // Dimension toolBarDimension = new Dimension(1000, 450);
@@ -17,11 +18,11 @@ public class PluginWindow extends JFrame implements Runnable {
         label = new JLabel();
         label.setIcon(icon);
         this.setTitle("QUANTUM Z3R0");
+        // Set Background
+        backGround = new ImageIcon("BackGround.png").getImage();
         // Init OscillatorPanels
         oscOne = new OscillatorPanel(Color.red);
-        oscOne.setBackground(Color.red);
         oscTwo = new OscillatorPanel(Color.blue);
-        oscTwo.setBackground(Color.blue);
         // Init Jframe Properties
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setPreferredSize(windowDimension);
@@ -39,16 +40,22 @@ public class PluginWindow extends JFrame implements Runnable {
     }
 
     @Override
+    public void paint(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.drawImage(backGround, 0, 0, null);
+    }
+
+    @Override
     public void run() {
         while (this.isActive()) {
-            // Create Graphics
-            Graphics gOne = oscOne.getGraphics();
-            Graphics gTwo = oscTwo.getGraphics();
-            // Run Paint Menthods Here
-            oscOne.paint(gOne);
-            oscTwo.paint(gTwo);
+            // // Create Graphics
+            // Graphics gOne = oscOne.getGraphics();
+            // Graphics gTwo = oscTwo.getGraphics();
+            // // Run Paint Menthods Here
+            // oscOne.paint(gOne);
+            // oscTwo.paint(gTwo);
             try {
-                Thread.sleep(250);
+                Thread.sleep(25);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
